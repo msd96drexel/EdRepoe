35d2c763b213246c6886cc33c8f2aab55c7d8582
Maria <shrizzleshreet101@yahoo.com>
2020-03-02 21:05:56 -0500

    view module and materials

---
+++ index.js
@@ -0,0 +1,29 @@
+$(".browse-entry").mouseenter(function() {
+    let id = $(this).attr('id');
+    $(`#${id} .ellipsis`).removeClass('hidden');
+});
+
+$(".browse-entry").mouseleave(function() {
+    let id = $(this).attr('id');
+    $(`#${id} .ellipsis`).addClass('hidden');
+});
+
+$(".ellipsis").click(function() {
+    let id = $(this).attr('id');
+    $(`#${id} .modal`).removeClass('hidden');
+    $(`#${id} .modal`).focus();
+});
+
+$(".modal").blur(function() {
+    $(".modal").addClass('hidden');
+});
+
+function showModuleInfo() {
+    $('#module').removeClass('hidden-gone');
+    $('#materials').addClass('hidden-gone');
+}
+
+function showMaterialsInfo() {
+    $('#module').addClass('hidden-gone');
+    $('#materials').removeClass('hidden-gone');
+}
